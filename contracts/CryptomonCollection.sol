204cd83fea3d258719c498419f2c6ef2d36fab48
HarshilMendpara <mendparaharshil@gmail.com>
2022-01-04 16:47:27 +0530

    Removed string array from function param
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- contracts/CryptomonCollection.sol
+++ contracts/CryptomonCollection.sol
@@ -6,21 +6,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CryptomonCollection is Ownable {
     struct MonCollection {
-        string[] names;
-        string[] images;
+        string names;
+        string images;
         string monType;
     }
 
     MonCollection[] public monCollections;
 
-    function createMonCollection(
-        string[] memory _names,
-        string[] memory _images,
-        string memory _monType
-    ) private onlyOwner returns (uint256) {
-        monCollections.push(
-            MonCollection({names: _names, images: _images, monType: _monType})
-        );
+    function createMonCollection(string memory _monType)
+        public
+        returns (uint256)
+    {
+        monCollections.push(MonCollection("Cryptomon1", "photo1", _monType));
         uint256 collectionId = monCollections.length - 1;
         return collectionId;
     }
--- test/cryptomon_collection.js
+++ test/cryptomon_collection.js
@@ -8,17 +8,18 @@ contract("CryptomonCollection", ([owner, alice, bob]) => {
   });
 
   it("should create a cryptomon collection", async () => {
-    const names = ["alice", "bob", "charlie"];
-    const types = "fire";
+    const names = ["Cryptomon1", "Cryptomon2", "Cryptomon3"];
+    const types = "hello";
     const photos = ["photo1", "photo2", "photo3"];
 
-    const results = await contractInstance.createMonCollection(
-      names,
-      photos,
-      types,
-      { from: alice }
-    );
+    const results = await contractInstance.createMonCollection(types, {
+      from: alice,
+    });
 
-    // assert.equal(results.name.length, 3);
+    let mon = await contractInstance.monCollections(0);
+
+    assert.equal(mon.names, "Cryptomon1");
+    assert.equal(mon.images, "photo1");
+    assert.equal(mon.monType, "hello");
   });
 });
