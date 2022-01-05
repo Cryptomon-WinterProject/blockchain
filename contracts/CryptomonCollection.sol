b4fc99665143245521b8857b8e5c8ba73d00a673
Akash Gupta <akash.gupta.anup@gmail.com>
2022-01-05 18:24:45 +0530

    Updated test and contracts for cryptomon_collection
    Co-authored-by: Harshil Mendpara <HarshilMendpara@users.noreply.github.com>
    Co-authored-by: Ujjawal gupta  <UjjawalGupta30@users.noreply.github.com>

--- contracts/CryptomonCollection.sol
+++ contracts/CryptomonCollection.sol
@@ -6,19 +6,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CryptomonCollection is Ownable {
     struct MonCollection {
-        string names;
-        string images;
+        string[] names;
+        string[] images;
         string monType;
     }
+    event CollectionCreated(string[] names, string[] images, string monType);
 
     MonCollection[] public monCollections;
 
-    function createMonCollection(string memory _monType)
-        public
-        returns (uint256)
-    {
-        monCollections.push(MonCollection("Cryptomon1", "photo1", _monType));
+    function createMonCollection(
+        string[] memory _names,
+        string[] memory _images,
+        string memory _monType
+    ) public returns (uint256) {
+        MonCollection memory monColl = MonCollection(
+            new string[](0),
+            new string[](0),
+            new string(0)
+        );
+        monCollections.push(monColl);
         uint256 collectionId = monCollections.length - 1;
+        for (uint256 i = 0; i < _names.length; i++) {
+            monCollections[collectionId].names.push(_names[i]);
+            monCollections[collectionId].images.push(_images[i]);
+        }
+        monCollections[collectionId].monType = _monType;
+        emit CollectionCreated(_names, _images, _monType);
         return collectionId;
+    }
+
+    function getMonCollection(uint256 _index)
+        public
+        view
+        returns (MonCollection memory)
+    {
+        return monCollections[_index];
     }
 }
--- test/cryptomon_collection.js
+++ test/cryptomon_collection.js
@@ -1,10 +1,10 @@
 const CryptomonCollection = artifacts.require("CryptomonCollection");
 
-contract("CryptomonCollection", ([owner, alice, bob]) => {
+contract("CryptomonCollection", ([owner, alice, bob, ash]) => {
   let contractInstance;
 
   beforeEach(async () => {
-    contractInstance = await CryptomonCollection.new({ from: owner });
+    contractInstance = await CryptomonCollection.new({ from: bob });
   });
 
   it("should create a cryptomon collection", async () => {
@@ -12,14 +12,21 @@ contract("CryptomonCollection", ([owner, alice, bob]) => {
     const types = "hello";
     const photos = ["photo1", "photo2", "photo3"];
 
-    const results = await contractInstance.createMonCollection(types, {
-      from: alice,
-    });
+    const results = await contractInstance.createMonCollection(
+      names,
+      photos,
+      types,
+      {
+        from: ash,
+      }
+    );
 
     let mon = await contractInstance.monCollections(0);
-
-    assert.equal(mon.names, "Cryptomon1");
-    assert.equal(mon.images, "photo1");
-    assert.equal(mon.monType, "hello");
+    const results2 = await contractInstance.getMonCollection(0);
+    console.log(results2);
+    // console.log(results.logs[0].args);
+    // assert.equal(mon.names, "Cryptomon1");
+    // assert.equal(mon.images, "photo1");
+    // assert.equal(mon.monType, "hello");
   });
 });
