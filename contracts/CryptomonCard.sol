b8a5de6db243e2d856af4d8080ffc86e0b887c3a
Akash Gupta <akash.gupta.anup@gmail.com>
2022-01-06 15:17:53 +0530

    Created IncreaseXP function and Tested
    Co-authored-by: Harshil Mendpara <HarshilMendpara@users.noreply.github.com>
    Co-authored-by: Ujjawal gupta  <UjjawalGupta30@users.noreply.github.com>

--- contracts/CryptomonCard.sol
+++ contracts/CryptomonCard.sol
@@ -16,8 +16,16 @@ contract CryptomonCard is CryptomonCollection {
     Cryptomon[] public cryptomons;
     event LogCryptomonCard(address indexed owner, uint256 monIndex);
 
+    modifier checkCardOwner(uint256 _monIndex) {
+        require(
+            cryptomons[_monIndex].owner == msg.sender,
+            "You are not the owner of this card"
+        );
+        _;
+    }
+
     function createCryptomonCard(uint256 _monIndex) public {
-        cryptomons.push(Cryptomon(_monIndex, 1, 0, 0, msg.sender));
+        cryptomons.push(Cryptomon(_monIndex, 9, 1, 100, msg.sender));
         emit LogCryptomonCard(msg.sender, _monIndex);
     }
 
@@ -27,5 +35,34 @@ contract CryptomonCard is CryptomonCollection {
         returns (Cryptomon memory)
     {
         return cryptomons[_monIndex];
+    }
+
+    function increaseXP(uint256 _monIndex, uint16 _XPToIncrease)
+        public
+        checkCardOwner(_monIndex)
+    {
+        uint16 XP = cryptomons[_monIndex].XP;
+        uint16 noOfEvolution = uint16(
+            monCollections[cryptomons[_monIndex].monIndex].names.length
+        );
+        XP = XP + _XPToIncrease;
+        uint16 rangeOfXP = 80 + uint16(cryptomons[_monIndex].monLevel) * 20;
+        if (XP >= rangeOfXP) {
+            XP = XP - rangeOfXP;
+            cryptomons[_monIndex].monLevel = cryptomons[_monIndex].monLevel + 1;
+            uint8 monLevel = cryptomons[_monIndex].monLevel;
+
+            uint256 evolution = uint256(
+                1 +
+                    ((uint256(noOfEvolution) - 1) * 10 * uint256(monLevel)) /
+                    (100 + 9 * uint256(monLevel))
+            );
+            if (evolution > uint256(cryptomons[_monIndex].evolution)) {
+                cryptomons[_monIndex].evolution =
+                    cryptomons[_monIndex].evolution +
+                    1;
+            }
+        }
+        cryptomons[_monIndex].XP = XP;
     }
 }
