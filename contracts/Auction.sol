bf75122d5c1df7466ab345b394da54fb11955c53
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-01-06 21:49:07 +0530

    Created Auction Sol
    Co-authored-by: Ayush-019 <Ayush-019@users.noreply.github.com>

---
+++ contracts/Auction.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Auction {
+  constructor() public {
+  }
+}
