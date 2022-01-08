b9f56c88028c7ae52bd1d97ad35d6113619f0e25
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-01-08 22:46:35 +0530

    completed auction contract

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,9 +2,23 @@
 pragma solidity >=0.4.22 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./AuctionMons.sol";
 
-contract Auction {
-  constructor() public {
-  }
+contract Auction is AuctionMons {
+    mapping(address => uint256) MonCoins;
+    address owner;
+
+    event NewBid(uint256 _cardIndex, address _bidder, uint256 _bidAmount);
+
+    function bid(uint256 _cardIndex) public payable beforeEndTime {
+        require(
+            MonCoins[msg.sender] + msg.value >
+                auctionCards[_cardIndex].highestBid
+        );
+        auctionCards[_cardIndex].highestBidder = msg.sender;
+        auctionCards[_cardIndex].highestBid = MonCoins[msg.sender] + msg.value;
+        MonCoins[msg.sender] += msg.value;
+
+        emit NewBid(_cardIndex, msg.sender, msg.value);
+    }
 }
