3a9b1b15463c8a70d1c0a1815ceb1b3914314187
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-01-08 22:47:32 +0530

    make auction test

--- test/auction.js
+++ test/auction.js
@@ -1,8 +1,32 @@
-const auction = artifacts.require("auction");
+const Auction = artifacts.require("Auction");
+const { shouldThrow, advanceTime } = require("./helpers/utils");
 
-contract("auction", function (/* accounts */) {
-  it("should assert true", async function () {
-    await auction.deployed();
-    return assert.isTrue(true);
+contract("Auction", function ([ownerAddress, alice, bob]) {
+  let contractInstance;
+
+  beforeEach(async () => {
+    contractInstance = await Auction.new({ from: ownerAddress });
+    await contractInstance.addToAuction(1, 1, { from: alice });
+  });
+
+  it("should bid", async () => {
+    const { logs } = await contractInstance.bid(1, { from: bob, value: 2 });
+
+    assert.equal(logs[0].args._itemId, 1);
+    assert.equal(logs[0].args._bidder, bob);
+    assert.equal(logs[0].args._bid, 2);
+
+    const item = await contractInstance.items(1);
+    assert.equal(item.highestBid, 2);
+    assert.equal(item.highestBidder, bob);
+  });
+
+  it("shouldn't allow bidding for lower than current highest bid", async () => {
+    await shouldThrow(contractInstance.bid(1, { from: bob, value: 0.5 }));
+  });
+
+  it("shouldn't allow bidding after end time", async () => {
+    await advanceTime(2 * 24 * 60 * 60);
+    await shouldThrow(contractInstance.bid(0, { from: bob, value: 2 }));
   });
 });
