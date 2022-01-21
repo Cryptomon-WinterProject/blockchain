1ffa5c8c73cd07e3782081e840ae9a6382a62823
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-01-21 14:05:12 +0530

    Write the shouldthrow function

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -7,7 +7,25 @@ async function shouldThrow(promise) {
     return;
   }
 }
-
+const advanceTime = (time) => {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "evm_increaseTime",
+        params: [time],
+        id: new Date().getTime(),
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
+};
 module.exports = {
   shouldThrow,
+  advanceTime,
 };
