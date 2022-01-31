a943e01e3b9168ae9df754fee1ef985ec63acda9
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-01-31 18:43:23 +0530

    created function store and get bid amount

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -5,20 +5,31 @@ pragma experimental ABIEncoderV2;
 import "./AuctionMons.sol";
 
 contract Auction is AuctionMons {
-    mapping(address => uint256) MonCoins;
-    address cardOwner;
+    mapping(address => uint256) monCoinBid;
+    address CardOwner;
 
     event NewBid(uint256 _cardIndex, address _bidder, uint256 _bidAmount);
 
     function bid(uint256 _cardIndex) public payable beforeEndTime {
         require(
-            MonCoins[msg.sender] + msg.value >
+            monCoinBid[msg.sender] + msg.value >
                 auctionCards[_cardIndex].highestBid
         );
         auctionCards[_cardIndex].highestBidder = msg.sender;
-        auctionCards[_cardIndex].highestBid = MonCoins[msg.sender] + msg.value;
-        MonCoins[msg.sender] += msg.value;
+        auctionCards[_cardIndex].highestBid =
+            monCoinBid[msg.sender] +
+            msg.value;
+        monCoinBid[msg.sender] += msg.value;
 
         emit NewBid(_cardIndex, msg.sender, msg.value);
+    }
+
+    function getBidAmount(uint256 amount) public payable {
+        require(msg.value == amount);
+    }
+
+    function storeBidAmount() public payable {
+        address payable add1 = payable(msg.sender);
+        add1.transfer(address(this).balance);
     }
 }
