49bc4aa68ae710dd72017c449a23f01bcfb59db1
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-02-01 18:43:50 +0530

    Completed

---
+++ contracts/Store.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "./Auction.sol";
+
+contract Store is Auction {
+    function buyCryptomons(uint256 _id, uint256 _price) public {
+        require(users[msg.sender].monCoinBalance >= _price);
+        cryptomons[_id].owner = msg.sender;
+        users[msg.sender].monCoinBalance -= _price;
+    }
+}
